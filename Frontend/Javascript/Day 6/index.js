50e2301c8ce8f1f2ccec37345c0eeb2210db6d9f
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-10-01 23:23:05 +0530

    Update index.js

--- Frontend/Javascript/Day 6/index.js
+++ Frontend/Javascript/Day 6/index.js
@@ -19,7 +19,7 @@
 // const b = str.toLowerCase();
 // console.log(b); 
 
-const str = `Hello Coder Army Coder`;
+const str = `Hello Coder Army Coder`
 
 
 // console.log(str.indexOf('cod'));
@@ -89,3 +89,4 @@ console.log(dates.toString());
 
 console.log(dates);
 // 1759340204932: TimeStamp
+
