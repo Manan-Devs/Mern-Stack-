0d19f0149caa1708ee7e9a36da57d118aac75e2d
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-10-16 23:15:05 +0530

    Update index.js

--- Frontend/Javascript/Day 15/index.js
+++ Frontend/Javascript/Day 15/index.js
@@ -5,7 +5,7 @@
 
 
 
-const element = document.getElementById("first")
+const element = document.getElementById("first");
 
 // element.onclick = function handleClick() {
 // // element.textContent = "Strike is Coming";
@@ -112,4 +112,5 @@ let a = {
 
 a.greet(10);
 a.greet(20);
+
 
