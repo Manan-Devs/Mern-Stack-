957e09fbf7633ed16c3c7fdf86d161ecd8bec95e
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-10-29 22:55:28 +0530

    Update index.js

--- Frontend/Javascript/Day 19/index.js
+++ Frontend/Javascript/Day 19/index.js
@@ -1,4 +1,103 @@
+// console.log("Hello, World");
 
+
+// // code 
+
+// const p1 = fetch("https://api.github.com/users");
+
+
+
+// // fulfilled , reject 
+
+// const p2 = p1.then((response) => {
+//     // console.log(response.json());
+//      return response.json();
+// });
+
+
+// p2.then((data) => {
+//     console.log(data);
+      
+// });
+
+
+// const p1 = new Promise((resolve,reject) => {
+//        resolve({
+//         name:"Manan",
+//         age:17
+//       });
+// })     
+// // console.log(p1);
+// p1.then((response) => {
+//      console.log(response);
+// }) .catch((error) => {
+//      console.log(error);
+// });
+
+
+fetch("https://api.github.com/users")
+.then((response) => {
+     if(!response.ok){
+         throw new Error("Data is not present in Server");
+         
+     }
+      return response.json();
+})
+.then((data)=> {
+    // console.log(data); 
+   const parent = document.getElementById("first");
+   for(let i = 0; i < data.length; i++){
+   const image  =  document.createElement('img');
+   image.src = data[i].avatar_url;
+   image.style.height = "200px";
+   image.style.height = "200px";
+   parent.append(image);
+}
+})
+.catch((error) => {
+      const parent = document.getElementById("first");
+      parent.textContent = error.message;
+})
+
+
+
+
+
+
+// // console.log("Hello,World End");
+
+
+// const j1 = {
+//      name:"Rohit",
+//      age:30,
+//      address:"dwarka",
+//      c:undefined
+// }
+
+
+// // convert object to JSON 
+
+// // const jsonFormat = JSON.stringify(j1);
+ 
+
+
+// // console.log(jsonFormat);
+
+
+// // JSON is just a string like this : 
+
+// const jsonFormat = `{
+//     "name":"Rohit",
+//     "age": "30",
+//     "address":"dwarka"
+// }`;
+
+
+// // convert into javascript object
+
+// const JsObject = JSON.parse(jsonFormat);
+
+// console.log(JsObject);
 
 
 
@@ -129,6 +228,7 @@ placedOrder(orderDetail)
 .catch((error) => {
     console.log("Error", error);
 });
+
 
 
 
