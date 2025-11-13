51e979c2d8f436547097e255f220a73a93a7d3c8
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-11-13 23:06:30 +0530

    Add files via upload

---
+++ Frontend/Javascript/Day 21/index.js
@@ -0,0 +1,27 @@
+// Prototypes and classes 
+
+const obj = {
+      name:"Rohit",
+      age:38,
+      greet:function() {
+         console.log("Hello,ji");
+      }
+};
+
+// console.log(obj.greet());
+
+// obj.greet();
+
+// console.log(obj.hasOwnProperty("names"));
+
+const arr = [10,20,30];
+
+arr.length;
+
+const obj2 = {
+     account: 30
+}
+
+obj2.__proto__ = obj;
+
+console.log(obj2.name);
