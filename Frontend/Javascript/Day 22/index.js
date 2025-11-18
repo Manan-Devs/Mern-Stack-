1845f1ce0a5754334183515c0539d8255896902a
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-11-18 23:14:33 +0530

    Update index.js

--- Frontend/Javascript/Day 22/index.js
+++ Frontend/Javascript/Day 22/index.js
@@ -1,1 +1,48 @@
+unction greet() {
+      console.log(`hi ${this.name}`);
+}
 
+function incrementAge(value,name,) {
+      this.age+=value;
+      this.name = name;
+      console.log(this.age);
+      console.log(this.name);
+      
+}
+
+const user = {
+     name:"Rohit",
+     age:30
+}
+
+const user2 = {
+     name:"Mohit",
+     age:10
+}
+
+// greet.call(user);
+// greet.call(user2);
+
+// incrementAge.call(user2,10,"Mohan");
+
+// apply
+incrementAge.apply(user2,[10,"Mohan"]);
+
+
+// bind 
+const incr = incrementAge.bind(user2,10,"Mohan");
+
+incr();
+
+// this === user
+
+// user2.greet;  // When we call our method in our object to , it checks who invokes this method and so that object is referred by this Keyword
+
+"use strict" 
+
+// function greet() {
+//      console.log(this);
+// }
+
+// greet()
+ 
