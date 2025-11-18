3137a41a8d881bf100610087e6e7398efdfda9be
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-11-18 23:14:53 +0530

    Update index.js

--- Frontend/Javascript/Day 22/index.js
+++ Frontend/Javascript/Day 22/index.js
@@ -1,4 +1,42 @@
-unction greet() {
+// this call apply bind 
+// this: class object method 
+// strict mode vs non strict mode 
+
+// global Object : in Browser: Window and in Node.js : Global 
+
+// globalThis : points to global object of the environment . it's the universal global object working in all environments
+// 'use strict';
+
+// normal function: without strict mode, this will point to global object
+// in strict mode it will point to undefined 
+
+// var a = 10;
+// var b = 20; Source of Bug 
+
+// console.log(a,b);
+
+// function greet(name1,name2) {   
+//     console.log(name1,name2); // Source of Bug 
+// }
+
+// greet("Rohit","Mohit");
+
+// document.getElementById("first")
+
+// console.log(globalThis);
+
+
+// Learn about This Keyword
+
+// this keyword in global scope : NodeJS(empty Object) , in browser it will point to global object
+// 'use strict'
+// console.log(this);
+
+
+// Function 
+// 100 user: greet function : 100*memory , code 
+
+function greet() {
       console.log(`hi ${this.name}`);
 }
 
