d744a115cba5d97fe26ab73d52bfd8feed52b63a
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-11-26 23:40:24 +0530

    Update app.js

--- Frontend/React/Day 2/app.js
+++ Frontend/React/Day 2/app.js
@@ -7,8 +7,8 @@
 // JSX: looks like Html but it's not Html
 // JSX --> React.createElement() --> React Element(JS Object) --> Real DOM(HTML Element Content)
 
- const element = <h1 id="title">Hello Coder Army</h1>;
- console.log(element);
+//  const element = <h1 id="title">Hello Coder Army</h1>;
+//  console.log(element);
  
 //  const element2 = (<div>
 //  <h1>Hell</h1>
@@ -18,20 +18,51 @@
 // React Element
 
 // const element3 = (<div>
-//      <h1>Hello,Coder Army</h1>
+//      <h1>Hello,Coder  Army</h1>
 //      <h2>Hello,Code r</h2>
 //     </div>
 // );
 
-// React Components 
+// React.createElement("ul", null, [React.createElement("ul",null,<li>Html</li>),
+// React.createElement("ul",null , <li>Css</li>,])
 
+
+const courses = ["Html","Css" , "Javascript","ReactJS"];
+
+// [<li>Html</li> , <li>Css</li> , <li>Javascript</li> , <li>ReactJS</li>]
+
+const element2 = (
+
+     <ul>       
+         {courses.map((course) => <li>{course}</li>)}
+     </ul>      
+)
+
+  function App() {
+       return( 
+           <h1>Hello, Coder Army</h1>
+       )
+  }         
+  
+  const element = <App />;
+
+// React Components 
+     
 // function App(name) {
       
 //     return (
 //          <h1>Hello, Coder Army {name} </h1>
 //     );
 // }   
+    // text/element : Javascript ka expression aap iske andar likh sakte hain
+    // Number,string , true , false, array , null , undefined ,array, object 
 
+    // Number, string , array 
+    // true false , null , undefined (render honge lekin kuch dikhayi nahi dega);
+    // Object : Error 
+    //  const age = 12;
+    //  const isLoggedIn = true;
+    //  const element = <h1>Hello,Coder {isLoggedIn ? <h2>Logged in </h2>: <h2>Kindly SignIn</h2>}</h1>
 
  const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -42,4 +73,4 @@
 //         React.createElement('h2',null,"h1");
 //  )
 
-// root.render(App(3500));
+root.render(element);
